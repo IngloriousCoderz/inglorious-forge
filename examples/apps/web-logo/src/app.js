8ad5fc65980cd35de259578bc70c3e1eee521e06
Matteo Antony Mistretta <antony.mistretta@gmail.com>
2026-02-07 20:26:10 +0100

    Wrap logo with border in example

--- examples/apps/web-logo/src/app.js
+++ examples/apps/web-logo/src/app.js
@@ -2,6 +2,7 @@ import { html } from "@inglorious/web"
 
 export const app = {
   render(api) {
-    return html`${api.render("logoForm")}${api.render("liveLogo")}`
+    return html`${api.render("logoForm")}
+      <div class="frame">${api.render("liveLogo")}</div>`
   },
 }
