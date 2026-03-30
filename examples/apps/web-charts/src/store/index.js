62ef0e9f31b80b5e81d4245ad9d6d39615ed048f
Vinicius Mantovani <vmantovani86@gmail.com>
2026-03-30 19:20:24 +0200

    refactor: simplify chart types and use realtime as a decorator

--- examples/apps/web-charts/src/store/index.js
+++ examples/apps/web-charts/src/store/index.js
@@ -1,31 +1,19 @@
-import { createStore } from "@inglorious/store"
-import {
-  areaChart,
-  barChart,
-  chart,
-  composedChart,
-  donutChart,
-  lineChart,
-  pieChart,
-} from "@inglorious/charts"
+import { createStore } from "@inglorious/web"
+import { chart } from "@inglorious/charts"
 import { withRealtime } from "@inglorious/charts/realtime"
 import { entities } from "./entities.js"
-import { middlewares } from "./middlewares.js"
-
-const lineRealtimeChart = withRealtime(lineChart)
 
 export const store = createStore({
   types: {
-    area: areaChart,
-    line: lineChart,
-    "line-rt": lineRealtimeChart,
-    bar: barChart,
-    composed: composedChart,
-    pie: pieChart,
-    donut: donutChart,
-    // Add chart object for composition methods
-    chart: chart,
+    area: chart,
+    bar: chart,
+    line: chart,
+    "area-rt": [chart, withRealtime],
+    "bar-rt": [chart, withRealtime],
+    "line-rt": [chart, withRealtime],
+    composed: chart,
+    pie: chart,
+    donut: chart,
   },
   entities,
-  middlewares,
 })
